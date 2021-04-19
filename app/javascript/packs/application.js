4b3ecbc992217ea70a25a897504d1ce8e8d11521
Efra <efrados@gmail.com>
2021-04-19 17:02:22 -0300

    Installed Bootrstrap Icons and fixed header view

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'bootstrap/js/src/collapse'
 import 'bootstrap/js/src/dropdown'
 
