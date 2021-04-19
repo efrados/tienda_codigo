db0e909d75605b08580f982f9c9540adbdd2dca2
Efra <efrados@gmail.com>
2021-04-19 13:09:12 -0300

    Bootstrap installed and working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'bootstrap/js/src/collapse'
+import 'bootstrap/js/src/dropdown'
 
 Rails.start()
 Turbolinks.start()
