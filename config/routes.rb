d87460fda4b49eea90056a9ff208d5171d388ce1
Efra <efrados@gmail.com>
2021-04-20 17:02:32 -0300

    Add link to Products index to accesss show prouct

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'static_pages/index'
   get 'static_pages/about'
   root to: "static_pages#index"
+  resources :products, only: :show
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
